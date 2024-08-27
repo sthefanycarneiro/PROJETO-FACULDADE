29bfcb569782006a28cbd44534407719d6218990
sthefany <sthef.carneiro@gmail.com>
2024-08-27 15:56:36 -0300

    tela de cadastro funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import GlobalStyle from './styles/global';
-import Form from './components/Form';
-import Grid from './components/Grid';
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -19,17 +20,30 @@ const Container = styled.div`
 const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
-        <Form />
-        <Grid/>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
-      <ToastContainer
-        autoClose={3000}
-        position="bottom-left" 
-      />
+      <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
     </>
   );
--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -17,6 +17,7 @@ const Table = styled.table`
 
 export const Thead = styled.thead``;
 
+export const Tbody = styled.tbody``;
 
 export const Tr = styled.tr``;
 
@@ -30,10 +31,34 @@ export const Th = styled.th`
   }
 `;
 
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
 const Grid = ({ users, setUsers, setOnEdit }) => {
-    const handleEdit = (item) => {
-      setOnEdit(item);
-    };
+  const handleEdit = (item) => {
+    setOnEdit(item);
+  };
+
+  const handleDelete = async (id) => {
+    await axios
+      .delete("http://localhost:8800/" + id)
+      .then(({ data }) => {
+        const newArray = users.filter((user) => user.id !== id);
+
+        setUsers(newArray);
+        toast.success(data);
+      })
+      .catch(({ data }) => toast.error(data));
+
+    setOnEdit(null);
+  };
 
   return (
     <Table>
@@ -46,6 +71,23 @@ const Grid = ({ users, setUsers, setOnEdit }) => {
           <Th></Th>
         </Tr>
       </Thead>
+      <Tbody>
+        {users.map((item, i) => (
+          <Tr key={i}>
+            <Td width="30%">{item.nome}</Td>
+            <Td width="30%">{item.email}</Td>
+            <Td width="20%" onlyWeb>
+              {item.fone}
+            </Td>
+            <Td alignCenter width="5%">
+              <FaEdit onClick={() => handleEdit(item)} />
+            </Td>
+            <Td alignCenter width="5%">
+              <FaTrash onClick={() => handleDelete(item.id)} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
     </Table>
   );
 };
